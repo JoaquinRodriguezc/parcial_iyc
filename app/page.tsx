6c0bdf7eb0c329663d91f3cf850f49616badb017
Joaquin Rodriguez <rodriguezcjoaquin02@gmail.com>
2025-09-20 17:40:53 -0300

    fix test

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1>TP 7 - Plan de desarrollo y mantenimiento. PRUEB</h1>
+        <h1>TP 7 - Plan de desarrollo y mantenimiento.</h1>
       </main>
     </div>
   );
